c3683e850b32fee8a35ba24bb926e28c3ad1df26
AFROZ002 <afrozcpr@gmail.com>
2026-03-30 05:29:43 +0530

    pehla commit

---
+++ src/com/prep/collections/CollectionMain.java
@@ -0,0 +1,10 @@
+
+
+package com.prep.collections;
+
+public class CollectionMain {
+    public static void main(String[] args) {
+
+        System.out.println("Alam bhai collections main aapka swagat hai");
+    }
+}
---
+++ src/com/prep/designpatterns/behavioral/BehavioralMain.java
@@ -0,0 +1,7 @@
+
+package com.prep.designpatterns.behavioral;
+public class BehavioralMain {
+    public static void main(String[] args) {
+        System.out.println("Alam bhai behavioral design patterns main aapka swagat hai");
+    }
+}
